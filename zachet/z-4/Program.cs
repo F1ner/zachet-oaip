b88df4efa6a6b0cae77d25021022eace0fad4cf2
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-03 13:30:15 +0300

    Add files via upload

---
+++ zachet/z-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address myAddress = new Address();
+
+            myAddress.Index = "1000";
+            myAddress.Country = "PMR";
+            myAddress.City = "Tiraspol";
+            myAddress.Street = "Mira 50";
+            myAddress.House = "20";
+            myAddress.Apartment = "4";
+
+            Console.WriteLine("Index: " + myAddress.Index);
+            Console.WriteLine("Country: " + myAddress.Country);
+            Console.WriteLine("City: " + myAddress.City);
+            Console.WriteLine("Street: " + myAddress.Street);
+            Console.WriteLine("House: " + myAddress.House);
+            Console.WriteLine("Apartment: " + myAddress.Apartment);
+
+            Console.ReadLine();
+        }
+    }
+}
