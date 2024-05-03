4aaea2e008d2b69df545050a807d1ffc3b9d4934
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-03 13:39:57 +0300

    Add files via upload

---
+++ zachet/z-5/Converter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_5
+{
+    class Converter
+    {
+        private double usdRate;
+        private double eurRate;
+        private double rubRate;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            usdRate = usd;
+            eurRate = eur;
+            rubRate = rub;
+        }
+
+        public double BYNtoUSD(double byn)
+        {
+            return byn / usdRate;
+        }
+
+        public double BYNtoEUR(double byn)
+        {
+            return byn / eurRate;
+        }
+
+        public double BYNtoRUB(double byn)
+        {
+            return byn / rubRate;
+        }
+
+        public double USDtoBYN(double usd)
+        {
+            return usd * usdRate;
+        }
+
+        public double EURtoBYN(double eur)
+        {
+            return eur * eurRate;
+        }
+
+        public double RUBtoBYN(double rub)
+        {
+            return rub * rubRate;
+        }
+    }
+
+}
---
+++ zachet/z-5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Инициализация курсов валют
+            double usdRate = 2.5;
+            double eurRate = 3.0;
+            double rubRate = 0.03;
+
+            Converter converter = new Converter(usdRate, eurRate, rubRate);
+
+            // Примеры конвертации
+            double amountInBYN = 100.0;
+            Console.WriteLine(amountInBYN + " BYN = " + converter.BYNtoUSD(amountInBYN) + " USD");
+            Console.WriteLine(amountInBYN + " BYN = " + converter.BYNtoEUR(amountInBYN) + " EUR");
+            Console.WriteLine(amountInBYN + " BYN = " + converter.BYNtoRUB(amountInBYN) + " RUB");
+
+            double amountInUSD = 50.0;
+            Console.WriteLine(amountInUSD + " USD = " + converter.USDtoBYN(amountInUSD) + " BYN");
+
+            double amountInEUR = 60.0;
+            Console.WriteLine(amountInEUR + " EUR = " + converter.EURtoBYN(amountInEUR) + " BYN");
+
+            double amountInRUB = 150.0;
+            Console.WriteLine(amountInRUB + " RUB = " + converter.RUBtoBYN(amountInRUB) + " BYN");
+
+            Console.ReadLine();
+        }
+    }
+}
