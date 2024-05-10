e790e3289f7db4d904b2098a3c8ad9945b940dc1
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-10 13:47:25 +0300

    40

---
+++ zachet/z-40/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_40
+{
+    class Program
+    {
+        // Статический метод для вычисления среднего арифметического всех целых чисел от m до n включительно
+        static double DoubleAverage(int m, int n)
+        {
+            int sum = 0;
+            int count = 0;
+            for (int i = m; i <= n; i++)
+            {
+                sum += i;
+                count++;
+            }
+            return (double)sum / count;
+        }
+
+        // Объявление делегата, соответствующего методу DoubleAverage
+        delegate double AverageCalculator(int m, int n);
+
+        static void Main(string[] args)
+        {
+            // Создание экземпляра делегата и присвоение ему ссылки на метод DoubleAverage
+            AverageCalculator averageCalculator = DoubleAverage;
+
+            // Применение делегата 3 раза с разными параметрами
+            double result1 = averageCalculator(1, 10);
+            double result2 = averageCalculator(20, 30);
+            double result3 = averageCalculator(-5, 5);
+
+            Console.WriteLine($"Среднее арифметическое всех целых чисел от 1 до 10: {result1}");
+            Console.WriteLine($"Среднее арифметическое всех целых чисел от 20 до 30: {result2}");
+            Console.WriteLine($"Среднее арифметическое всех целых чисел от -5 до 5: {result3}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ zachet/z-40/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("z-40")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("z-40")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6467cbd7-c521-4592-9289-e8cfe233f6f0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
