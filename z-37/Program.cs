99be6551c6cc7b5ca4fc0480e4c7bf8701a7753c
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-10 13:30:37 +0300

    Add files via upload

---
+++ z-37/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_37
+{
+    class Program
+    {
+        delegate double TriangleArea();
+
+        static void Main(string[] args)
+        {
+            // Создание объекта треугольника
+            Triangle triangle1 = new Triangle(3, 4, 5);
+            Triangle triangle2 = new Triangle(5, 6, 7);
+            Triangle triangle3 = new Triangle(8, 10, 12);
+
+            // Присвоение указателя на метод делегату
+            TriangleArea areaCalculator1 = triangle1.CalculateArea;
+            TriangleArea areaCalculator2 = triangle2.CalculateArea;
+            TriangleArea areaCalculator3 = triangle3.CalculateArea;
+
+            // Вычисление площади треугольников с использованием делегата
+            double area1 = areaCalculator1();
+            double area2 = areaCalculator2();
+            double area3 = areaCalculator3();
+
+            Console.WriteLine($"Площадь треугольника 1: {area1}");
+            Console.WriteLine($"Площадь треугольника 2: {area2}");
+            Console.WriteLine($"Площадь треугольника 3: {area3}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ z-37/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_37
+{
+    // Класс, представляющий треугольник
+    /// <summary>
+    /// Класс, представляющий треугольник.
+    /// </summary>
+    class Triangle
+    {
+        private double a, b, c;
+
+        /// <summary>
+        /// Делегат для вычисления площади треугольника.
+        /// </summary>
+        /// <returns>Возвращает площадь треугольника.</returns>
+        delegate double TriangleArea();
+
+        /// <summary>
+        /// Конструктор класса Triangle.
+        /// </summary>
+        /// <param name="sideA">Длина стороны A треугольника.</param>
+        /// <param name="sideB">Длина стороны B треугольника.</param>
+        /// <param name="sideC">Длина стороны C треугольника.</param>
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            a = sideA;
+            b = sideB;
+            c = sideC;
+        }
+
+        /// <summary>
+        /// Метод для вычисления площади треугольника.
+        /// </summary>
+        /// <returns>Возвращает площадь треугольника.</returns>
+        public double CalculateArea()
+        {
+            // Формула Герона для вычисления площади треугольника
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+    }
+}
