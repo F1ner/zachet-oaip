6c90af5d03c4ab968c41c217e3f3b1bfba05f7c6
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-10 13:42:23 +0300

    44

---
+++ zachet/z-44/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_44
+{
+    class Program
+    {
+       
+        
+            // Статический метод для вычисления суммы всех целых чисел от m до n включительно
+            static int IntSum(int m, int n)
+            {
+                int sum = 0;
+                for (int i = m; i <= n; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+
+            // Объявление делегата, соответствующего методу IntSum
+            delegate int SumCalculator(int m, int n);
+
+            static void Main(string[] args)
+            {
+                // Создание экземпляра делегата и присвоение ему ссылки на метод IntSum
+                SumCalculator sumCalculator = IntSum;
+
+                // Применение делегата 3 раза с разными параметрами
+                int result1 = sumCalculator(1, 10);
+                int result2 = sumCalculator(20, 30);
+                int result3 = sumCalculator(-5, 5);
+
+                Console.WriteLine($"Сумма всех целых чисел от 1 до 10: {result1}");
+                Console.WriteLine($"Сумма всех целых чисел от 20 до 30: {result2}");
+                Console.WriteLine($"Сумма всех целых чисел от -5 до 5: {result3}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+
---
+++ zachet/ангел/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1 
+
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Следующие за числом {x}) число - {x + 1}");
+            //Console.WriteLine($"Следующее за числом 20 число {x}) число - {x - 1}");
+            //Console.ReadKey();
+
+            //Задание 2
+            //string a = "Забыть";
+            //string b = "число";
+            //string c = "на";
+            //string d = "всегда";
+            //string e = "и";
+            //string space = " ";
+            //string x = string.Concat(a, space, c, b, space, e, space, c, d);
+            //Console.WriteLine($"{x}");
+            //Console.ReadLine();
+
+            //Задача 3
+            //Console.WriteLine("a= ");
+            //string a = Console.ReadLine();
+            //Console.WriteLine("b= ");
+            //string b = Console.ReadLine();
+            //Console.WriteLine("c= ");
+            //string c = Console.ReadLine();
+            //Console.WriteLine("d= ");
+            //string d = Console.ReadLine();
+            //Console.WriteLine("e= ");
+            //string e = Console.ReadLine();
+            //Console.WriteLine($"{a}");
+            //Console.WriteLine($"{b}");
+            //Console.WriteLine($"{c}\n{d}\n{e}");
+            //Console.ReadLine();
+
+            //Задача 4
+            //Console.WriteLine("\n\n\n\n\n");
+            //Console.WriteLine("        ############");
+            //Console.WriteLine("       ############");
+            //Console.WriteLine("      ############");
+            //Console.WriteLine("     ############");
+            //Console.WriteLine("    ############");
+            //Console.WriteLine("   ############");
+            //Console.WriteLine("  ############");
+            //Console.WriteLine(" ############");
+            //Console.WriteLine("############\n\n");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
