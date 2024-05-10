b25287201ef833ad9f89defc92bb4cdd2e041310
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-10 14:22:05 +0300

    47

---
+++ zachet/z-47/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем список для хранения заказов
+            List<Zakaz> zakazy = new List<Zakaz>();
+
+            // Цикл для ввода заказов с клавиатуры
+            while (true)
+            {
+                // Предлагаем пользователю ввести фамилию и проверяем, не ввел ли он "Q" для выхода из программы
+                Console.WriteLine("Введите фамилию (Q для выхода):");
+                string fam = Console.ReadLine();
+                if (fam.ToUpper() == "Q")
+                    break; // Если введено "Q", выходим из цикла
+
+                // Предлагаем пользователю ввести количество мест в номере
+                Console.WriteLine("Введите количество мест в номере:");
+                int size = int.Parse(Console.ReadLine());
+
+                // Предлагаем пользователю ввести уровень комфорта
+                Console.WriteLine("Введите комфортность:");
+                string comfort = Console.ReadLine();
+
+                // Создаем новый заказ и добавляем его в список заказов
+                Zakaz zakaz = new Zakaz(fam, size, comfort);
+                zakazy.Add(zakaz);
+            }
+
+            // Выводим информацию о всех заказах
+            foreach (var zakaz in zakazy)
+            {
+                zakaz.Show();
+            }
+
+            Console.ReadLine(); // Ожидание нажатия клавиши перед завершением программы
+        }
+
+    }
+}
---
+++ zachet/z-47/Zakaz.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_47
+{
+    class Zakaz
+    {
+        string fam;
+        int size;
+        string comfort;
+        /// <summary>
+        /// Фамилия клиента.
+        /// </summary>
+        public string Fam { 
+            get { return fam; }
+            set { fam = value; } 
+        }
+
+        /// <summary>
+        /// Количество мест в номере.
+        /// </summary>
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        /// <summary>
+        /// Уровень комфорта заказа.
+        /// </summary>
+        public string Comfort
+        {
+            get { return comfort; }
+            set { comfort = value; }
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Zakaz() { }
+
+        /// <summary>
+        /// Конструктор с 3 параметрами.
+        /// </summary>
+        /// <param name="fam">Фамилия клиента.</param>
+        /// <param name="size">Количество мест в номере.</param>
+        /// <param name="comfort">Уровень комфорта.</param>
+        public Zakaz(string fam, int size, string comfort)
+        {
+            Fam = fam;
+            Size = size;
+            Comfort = comfort;
+        }
+
+        /// <summary>
+        /// Конструктор с двумя параметрами.
+        /// </summary>
+        /// <param name="fam">Фамилия клиента.</param>
+        /// <param name="size">Количество мест в номере.</param>
+        public Zakaz(string fam, int size) : this(fam, size, "стандартный") { }
+
+        /// <summary>
+        /// Конструктор с одним параметром.
+        /// </summary>
+        /// <param name="fam">Фамилия клиента.</param>
+        public Zakaz(string fam) : this(fam, 1) { }
+
+        /// <summary>
+        /// Конструктор с одним параметром.
+        /// </summary>
+        /// <param name="size">Количество мест в номере.</param>
+        public Zakaz(int size) : this("неизвестно", size) { }
+
+        /// <summary>
+        /// Метод для отображения информации о заказе.
+        /// </summary>
+        public void Show()
+        {
+            Console.WriteLine($"Заказ: Фамилия - {Fam}, Количество мест - {Size}, Комфортность - {Comfort}");
+        }
+    }
+
+
+}
