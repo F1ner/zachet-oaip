2550ffbca591a740aa12ccc7e62b0db5eea8d42f
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-20 08:49:35 +0300

    42

---
+++ zachet/z-42/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем экземпляры класса Point с разными типами данных
+            Point<int> intPoint = new Point<int>(10, 20);
+            Point<uint> uintPoint = new Point<uint>(30, 40);
+            Point<double> doublePoint = new Point<double>(3.5, 4.5);
+
+            // Выводим значения внутренних полей
+            intPoint.Display();
+            uintPoint.Display();
+            doublePoint.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
